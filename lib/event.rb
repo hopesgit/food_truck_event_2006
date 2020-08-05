62316134c52324c77ad007e682d6171fec214a73
Hope Gochnour <hope.gochnour@gmail.com>
2020-08-05 10:57:12 -0600

    Update: Event: unfinished method total_inventory; added item_list method

--- lib/event.rb
+++ lib/event.rb
@@ -24,4 +24,23 @@ class Event
       truck.check_stock(item) > 0
     end
   end
+
+  def item_list
+    @food_trucks.flat_map do |truck|
+      truck.inventory.keys
+    end.uniq
+  end
+
+  def sorted_item_list
+  end
+
+  def total_inventory
+    item_list.reduce({}) do |result, item|
+      stock = @food_trucks.each do { |truck| truck.check_stock(item)} 
+      result[item] = {quantity: stock, food_trucks: food_trucks_that_sell(item)}
+    end
+  end
+
+  def overstocked_items
+  end
 end
